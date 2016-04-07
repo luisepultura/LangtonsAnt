80873da730cd8f4838ad2470719366c77aa17f93
Luis Urrea <Luis.Urrea@g2g3.com>
2016-04-07 13:29:19 +0100

    tweaks.

--- dist/langtonView.js
+++ dist/langtonView.js
@@ -10,6 +10,8 @@ var LangtonView = function LangtonView(options) {
     this.pixels = options.pixels;
     this.speed = options.speed;
     this.ants = options.ants;
+    this.grid = [];
+    this.context = options.canvas.getContext("2d");
     //     ctx.fillStyle = "green";
     // ctx.fillRect(10, 10, 100, 100);
 };
@@ -18,16 +20,34 @@ LangtonView.prototype = {
     createGrid: function createGrid() {
         this.canvas.width = this.size * this.pixels;
         this.canvas.height = this.size * this.pixels;
-        // for (let y = 0; y < size; y++) {
-        //     grid[y] = [];
-        //     for (let x = 0; x < size; x++) {
-        //         grid[y][x] = 0;
-        //     }
-        // }
+        for (var y = 0; y < this.size; y++) {
+            grid[y] = [];
+            for (var x = 0; x < this.size; x++) {
+                grid[y][x] = 0;
+            }
+        }
     },
-    updateView: function updateView() {},
+    next: function next() {
+        var _this = this;
+
+        this.ants.forEach(function (ant, index) {
+            if (!_this.isOutOfRange(ant)) {
+                var isBlack = !!_this.board[ant.x][ant.y];
+                if (isBlack) {
+                    _this.context.fillStyle = '#fff';
+                    _this.board[ant.x][ant.y] = 0;
+                    ant.turnLeft().forward();
+                } else {
+                    _this.context.fillStyle = '#000';
+                    _this.board[ant.x][ant.y] = 1;
+                    ant.turnRight().forward();
+                }
+            } else {
+                _this.ants.splice(index, 1);
+            }
+        });
+    },
     play: function play() {
-        var game = new Langton(createGrid(), ants);
         this.createGrid();
     }
 };
--- js/langtonView.js
+++ js/langtonView.js
@@ -10,6 +10,8 @@ var LangtonView = function(options) {
     this.pixels = options.pixels;
     this.speed = options.speed;
     this.ants = options.ants;
+    this.grid = [];
+    this.context = options.canvas.getContext("2d");
     //     ctx.fillStyle = "green";
     // ctx.fillRect(10, 10, 100, 100);
 };
@@ -18,20 +20,36 @@ LangtonView.prototype = {
     createGrid: function() {
         this.canvas.width = this.size * this.pixels;
         this.canvas.height = this.size * this.pixels;
-        // for (let y = 0; y < size; y++) {
-        //     grid[y] = [];
-        //     for (let x = 0; x < size; x++) {
-        //         grid[y][x] = 0;
-        //     }
-        // }
+        for (let y = 0; y < this.size; y++) {
+            grid[y] = [];
+            for (let x = 0; x < this.size; x++) {
+                grid[y][x] = 0;
+            }
+        }
     },
-    updateView: function() {
-
+    next: function() {
+        this.ants.forEach((ant, index) => {
+            if (!this.isOutOfRange(ant)) {
+                let isBlack = !!this.board[ant.x][ant.y];
+                if (isBlack) {
+                    this.context.fillStyle = '#fff';
+                    this.board[ant.x][ant.y] = 0;
+                    ant.turnLeft().forward();
+                }
+                else {
+                    this.context.fillStyle = '#000';
+                    this.board[ant.x][ant.y] = 1;
+                    ant.turnRight().forward();
+                }
+            }
+            else {
+                this.ants.splice(index, 1);
+            }
+        });
     },
-    play: function() {
-        let game = new Langton(createGrid(), ants);
+    play: function() {        
         this.createGrid();
-        
+
     }
 };
 
